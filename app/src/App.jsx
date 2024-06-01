e8769ae0c4edd56ca014a075567b25f083d97f6d
Zain <learnerstudent001@gmail.com>
2024-06-01 18:53:32 +0500

    feat: Redux Toolkit Configured

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,12 +1,38 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
-import conf from './conf/conf'
+import { useDispatch } from 'react-redux'
+import authService from './appwrite/auth'
+import { login, logout } from './store/authSlice'
+import { Header, Footer } from './components/index'
 
 function App() {
-  return (
-    <>
-    </>
-  )
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService.getCurrentUser().then((userData) => {
+      if (userData) {
+        dispatch(login({userData}))
+      } else {
+        dispatch(logout())
+      }
+    })
+    .finally(() => setLoading(false))
+  }, []);
+
+
+
+  return ! loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div>
+        <Header/>
+        <main>
+          {/* <outlet/> */}
+        </main>
+        <Footer/>
+      </div>
+    </div>
+  ) : null
 }
 
 export default App
